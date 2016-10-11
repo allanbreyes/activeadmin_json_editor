ed4ee5b88bbc513f0e779834c8a787906a1af7f8
Mehernosh Bhathena <mehernosh47@gmail.com>
2016-10-11 11:28:56 +0530

    Railties 5.0 + jsonb fix (#3)
    * Bumping railties allowable version
    
    * support JSONB types
    
    * Add jsonb support explicitly

--- lib/activeadmin/resource_dsl.rb
+++ lib/activeadmin/resource_dsl.rb
@@ -7,7 +7,7 @@ module ActiveAdmin
       before_save do |object, _args|
         request_namespace = object.class.name.underscore.tr('/', '_')
         if params.key? request_namespace
-          object.class.columns_hash.select { |_key, attr| attr.type == :json }.keys.each do |key|
+          object.class.columns_hash.select { |_key, attr| attr.type.in? [:json, :jsonb] }.keys.each do |key|
             next unless params[request_namespace].key? key
             json_data = params[request_namespace][key]
             data = if json_data == 'null' || json_data.blank?
